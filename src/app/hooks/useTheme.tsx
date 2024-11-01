9da0aad0078da0d812ce947c62519117919c9a01
Yesi Molina <yesi@yesimolina.dev>
2024-11-01 17:31:37 -0600

    build error :bug:

--- src/app/hooks/useTheme.tsx
+++ src/app/hooks/useTheme.tsx
@@ -2,27 +2,41 @@
 import { useEffect, useState } from "react";
 
 const useTheme = () => {
-  const savedTheme = localStorage.getItem("theme");
-  const [theme, setTheme] = useState<"light" | "dark">(
-    (savedTheme as "light" | "dark") || "light",
-  );
+  const [theme, setTheme] = useState<"light" | "dark">(() => {
+    if (typeof window !== "undefined") {
+      const savedTheme = localStorage.getItem("theme");
+      if (savedTheme) {
+        return savedTheme as "light" | "dark";
+      } else {
+        const prefersDark = window.matchMedia(
+          "(prefers-color-scheme: dark)",
+        ).matches;
+        return prefersDark ? "dark" : "light";
+      }
+    }
+    return "light"; // Default theme during SSR
+  });
 
   useEffect(() => {
-    const savedTheme = localStorage.getItem("theme");
-    if (savedTheme) {
-      setTheme(savedTheme as "light" | "dark");
-    } else {
-      const prefersDark = window.matchMedia(
-        "(prefers-color-scheme: dark)",
-      ).matches;
-      setTheme(prefersDark ? "dark" : "light");
+    if (typeof window !== "undefined") {
+      const savedTheme = localStorage.getItem("theme");
+      if (savedTheme) {
+        setTheme(savedTheme as "light" | "dark");
+      } else {
+        const prefersDark = window.matchMedia(
+          "(prefers-color-scheme: dark)",
+        ).matches;
+        setTheme(prefersDark ? "dark" : "light");
+      }
     }
   }, []);
 
   useEffect(() => {
-    document.documentElement.classList.remove("light", "dark");
-    document.documentElement.classList.add(theme);
-    localStorage.setItem("theme", theme);
+    if (typeof window !== "undefined") {
+      document.documentElement.classList.remove("light", "dark");
+      document.documentElement.classList.add(theme);
+      localStorage.setItem("theme", theme);
+    }
   }, [theme]);
 
   const toggleTheme = () => {
